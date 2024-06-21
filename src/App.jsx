b911e2784f5ed667dd5b62ae7894b6dcea4f9852
kom-senapati <komnoob123@gmail.com>
2024-06-21 09:13:42 +0530

    🎨 theme setup done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,40 @@ import { Route, Switch } from "wouter";
 import Portfolio from "./pages/Portfolio";
 import ProjectGallery from "./pages/ProjectGallery";
 import LinkTree from "./pages/Linktree";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 const App = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
-    <Switch>
-      <Route path="/" component={Portfolio} />
-      <Route path="/projects" component={ProjectGallery} />
-      <Route path="/linktree" component={LinkTree} />
+    <>
+      <div className="h-8 pt-5 flex flex-row gap-5 items-center justify-center">
+        <h1>Select a theme : </h1>
+        <button
+          data-set-theme="forest"
+          data-act-class="ACTIVECLASS"
+          className="btn btn-neutral btn-sm"
+        >
+          DARK
+        </button>
+        <button
+          data-set-theme="emerald"
+          data-act-class="ACTIVECLASS"
+          className="btn btn-neutral btn-sm"
+        >
+          LIGHT
+        </button>
+      </div>
+      <Switch>
+        <Route path="/" component={Portfolio} />
+        <Route path="/projects" component={ProjectGallery} />
+        <Route path="/linktree" component={LinkTree} />
 
-      <Route>404: No such page!</Route>
-    </Switch>
+        <Route>404: No such page!</Route>
+      </Switch>
+    </>
   );
 };
 
